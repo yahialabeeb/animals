da67e8e7bbfde4410e2798671fd46d8c41b42699
yahialabeeb <yahia.labeeb@gmail.com>
2021-08-30 05:24:34 +0300

    editing

---
+++ horned-beasts/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import Header from './contant/Header';
+import Main from './contant/Main';
+import Footer from './contant/Footer';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+
+}
+
+export default App;
